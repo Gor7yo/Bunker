397ebea27bff8b7ca7214c199bd0ce052398700a
gor2210 <gor22.11@bk.ru>
2025-10-29 13:49:05 +0300

    -

--- src/AppRoutes/AppRoutes.jsx
+++ src/AppRoutes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Home } from "../pages/Home/Home"
 import { JoinRoom } from "../pages/JoinRoom/JoinRoom"
 import { NotFound } from "../pages/NotFound/NotFound"
 import { Lobby } from "../pages/Lobby/Lobby"
+import { AdminPanel } from "../pages/AdminPanel/AdminPanel"
 
 export const AppRoutes = ({ toggleCashback }) => {
 	return (
@@ -11,6 +12,7 @@ export const AppRoutes = ({ toggleCashback }) => {
 			<Route path="/" element={<Navigate to={'/home'}/>} />
 			<Route path="/join-room" element={<JoinRoom toggleCashback={toggleCashback} />} />
 			<Route path="/lobby" element={<Lobby />} />
+			<Route path="/admin-panel" element={<AdminPanel />} />
 			<Route path="*" element={<NotFound />}/>
 		</Routes>
 	)
