458b8c1a92dd386dc6777712c1bd977c78a5a731
gor2210 <gor22.11@bk.ru>
2025-10-30 20:02:11 +0300

    -

--- src/pages/AdminPanel/AdminPanel.jsx
+++ src/pages/AdminPanel/AdminPanel.jsx
@@ -252,7 +252,7 @@ export const AdminPanel = () => {
                         title={`Кикнуть игрока ${player.name}`}
                         disabled={!connected || !joined}
                       >
-                        Kick asdasdasdas
+                        Kick asdasdasdass
                       </button>
                     </div>
                   )}
