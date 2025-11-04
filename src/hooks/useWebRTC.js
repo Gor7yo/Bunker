3645c1bc07e8896767b2f74868a7ec7b2f8f6540
gor2210 <gor22.11@bk.ru>
2025-11-04 17:46:48 +0300

    -

--- src/hooks/useWebRTC.js
+++ src/hooks/useWebRTC.js
@@ -16,8 +16,8 @@ export const useWebRTC = (ws, playerId, players) => {
 
   const getAdaptiveVideoParams = (peerCount, isHidden) => {
     // Consistent high quality for all peers
-    let maxBitrate = 1_200_000; // 1.2 Mbps
-    let maxFramerate = 24;
+    let maxBitrate = 1_500_000; // 1.2 Mbps
+    let maxFramerate = 30;
     let scaleResolutionDownBy = 1;
 
     if (isHidden) {
